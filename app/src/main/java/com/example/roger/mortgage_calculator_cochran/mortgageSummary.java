02b8635f14fec828db003624e9e34ef229511a89
cochransd <cochransd@g.cofc.edu>
2017-02-10 13:27:02 -0800

    Initial commit

---
+++ app/src/main/java/com/example/roger/mortgage_calculator_cochran/mortgageSummary.java
@@ -0,0 +1,43 @@
+package com.example.roger.mortgage_calculator_cochran;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class mortgageSummary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mortgage_summary);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_mortgage_summary);
+        layout.addView(textView);
+    }
+
+
+}
+
